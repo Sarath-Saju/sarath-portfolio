050277b21aa6240c50d0e7abe5d7300d2bcedd1d
Sarath-Saju <sarathsajuchry@gmail.com>
2026-04-01 00:47:53 +0530

    fix port issue

--- backend/server.js
+++ backend/server.js
@@ -76,3 +76,8 @@ app.get("/messages", async (req, res) => {
 app.get("/", (req, res) => {
   res.send("Backend running ✅");
 });
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
